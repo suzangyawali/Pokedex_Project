a93c45cb3e2681135ad722d63c09062c958b0493
suzangyawali <sujangyawali17@gmail.com>
2024-08-21 15:17:24 +0530

    Added CSS to Pokedex

--- src/Components/Pokemon/Pokemon.jsx
+++ src/Components/Pokemon/Pokemon.jsx
@@ -1,10 +1,10 @@
-
+import "./Pokemon.css";
 function Pokemon({name,image}){
 
     return (
-        <div>
-            <div>{name}</div>
-            <div>{<img src={image}/>}</div>
+        <div className="pokemon">
+            <div className="pokemon-name">{name}</div>
+            <div >{<img src={image} className="pokemon-img" />}</div>
         </div>
     )
 }
